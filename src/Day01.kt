fffe352a9f2c5dc80f4129dd3610c1c60b83c3c9
Yg0R2 <yg0r2@yahoo.com>
2021-12-01 19:15:12 +0100

    feature: count measurement increase

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val parsedInput = input.toIntList()
+
+        return parsedInput
+            .dropLast(1)
+            .filterIndexed { index, element -> element < parsedInput[index + 1] }
+            .size
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val parsedInput = input.toIntList()
+
+        return parsedInput
+            .dropLast(3)
+            .filterIndexed { index, _ -> parsedInput.subList(index, index + 3).sum() < parsedInput.subList(index + 1, index + 1 + 3).sum() }
+            .size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,13 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 /**
+ * Converts a list of Sting into a list of Int.
+ */
+fun List<String>.toIntList(): List<Int> = this
+    .filter { it.isNotBlank() }
+    .map { it.toInt() }
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
