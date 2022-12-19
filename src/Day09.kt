2eabef98f5471820bc01afccbe72fb866f6114be
Yg0R2 <yg0r2@yahoo.com>
2022-12-19 13:53:22 +0100

    feature: 2022 Day 09 - Rope Bridge - Part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -3,7 +3,7 @@ import kotlin.math.abs
 fun main() {
 
     fun part1(input: List<String>): Int {
-        val rope = Rope()
+        val rope = Rope(2)
 
         input.initSteps()
             .forEach { rope.move(it.first, it.second) }
@@ -12,7 +12,12 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val rope = Rope(10)
+
+        input.initSteps()
+            .forEach { rope.move(it.first, it.second) }
+
+        return rope.tailCoordinates.size
     }
 
     // test if implementation meets criteria from the description, like:
@@ -21,47 +26,51 @@ fun main() {
         checkResult("test part1", 13) {
             part1(testInput)
         }
-//        checkResult("test part2", 1) {
-//            part2(testInput)
-//        }
+        checkResult("test part2", 1) {
+            part2(testInput)
+        }
+        checkResult("test2 part2", 36) {
+            part2(readInput("${this}_test2"))
+        }
 
         val input = readInput(this)
         println(part1(input))
-//        println(part2(input))
+        println(part2(input))
     }
 
 }
+
+private data class Knot(
+    val x: Int,
+    val y: Int
+)
 
 private enum class Movements {
     L, R, U, D
 }
 
-private class Rope {
+private class Rope(
+    length: Int
+) {
 
-    // x, y
-    private var head: Pair<Int, Int> = Pair(0, 0)
+    private val knots: MutableList<Knot> = MutableList(length) {
+        Knot(0, 0)
+    }
 
-    private var tail: Pair<Int, Int> = Pair(0, 0)
-
-    val tailCoordinates: MutableSet<Pair<Int, Int>> = mutableSetOf<Pair<Int, Int>>()
-        .also { it.add(tail) }
+    val tailCoordinates: MutableSet<Knot> = mutableSetOf<Knot>()
+        .also { it.add(knots[knots.size - 1]) }
 
     fun move(movement: Movements, amount: Int) {
         IntRange(1, amount).forEach { _ ->
-            head = head.move(movement)
+            moveHead(movement)
 
-            if (tailHasToMove()) {
-                tail = tail.copy(
-                    first = tail.first + getMovementDirection(tail.first - head.first),
-                    second = tail.second + getMovementDirection (tail.second - head.second)
-                )
+            moveTail()
 
-                tailCoordinates.add(tail)
-            }
+            tailCoordinates.add(knots[knots.size - 1])
         }
     }
 
-    private fun getMovementDirection(diff: Int) =
+    private fun getTailDirection(diff: Int) =
         if (diff == 0) {
             0
         } else if (diff < 0) {
@@ -70,28 +79,44 @@ private class Rope {
             -1
         }
 
-    private fun Pair<Int, Int>.move(movement: Movements): Pair<Int, Int> =
-        when (movement) {
-            Movements.L -> {
-                copy(first = first - 1)
-            }
+    private fun moveHead(movement: Movements) {
+        knots[0] = with(knots[0]) {
+            when (movement) {
+                Movements.L -> {
+                    copy(x = x - 1)
+                }
 
-            Movements.R -> {
-                copy(first = first + 1)
-            }
+                Movements.R -> {
+                    copy(x = x + 1)
+                }
 
-            Movements.U -> {
-                copy(second = second - 1)
-            }
+                Movements.U -> {
+                    copy(y = y - 1)
+                }
 
-            Movements.D -> {
-                copy(second = second + 1)
+                Movements.D -> {
+                    copy(y = y + 1)
+                }
             }
         }
+    }
 
-    private fun tailHasToMove(): Boolean =
-        (abs(tail.first - head.first) == 2) ||
-                (abs(tail.second - head.second) == 2)
+    private fun moveTail() {
+        IntRange(1, knots.size - 1).forEach {
+            if (tailHasToMove(it)) {
+                knots[it] = with(knots[it]) {
+                    copy(
+                        x = x + getTailDirection(x - knots[it - 1].x),
+                        y = y + getTailDirection(y - knots[it - 1].y)
+                    )
+                }
+            }
+        }
+    }
+
+    private fun tailHasToMove(currentIndex: Int): Boolean =
+        (abs(knots[currentIndex].x - knots[currentIndex - 1].x) == 2) ||
+                (abs(knots[currentIndex].y - knots[currentIndex - 1].y) == 2)
 
 }
 
